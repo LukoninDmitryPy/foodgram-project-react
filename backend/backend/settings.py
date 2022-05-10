d9586b0268c6fb1cc48dc5e9f6ffbc52519814eb
LukoninDmitryPy <liondmitry4@gmail.com>
2022-05-10 15:40:32 +0300

    1.1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,9 +120,9 @@ REST_FRAMEWORK = {
 DJOSER = {
     'LOGIN_FIELD': 'email',
     'SERIALIZERS': {
-        'user_create': 'api.serializers.CustomUserCreateSerializer',
-        'user': 'api.serializers.CustomUserSerializer',
-        'current_user': 'api.serializers.CustomUserSerializer',
+        'user_create': 'users.serializers.CustomUserCreateSerializer',
+        'user': 'users.serializers.CustomUserSerializer',
+        'current_user': 'users.serializers.CustomUserSerializer',
     },
     'PERMISSIONS': {
         'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
